00e86ae7d2876e66d74f6153908c2c6bacbbcf45
unknown <dyllucar@gmail.com>
2020-11-03 14:56:24 -0600

    update font end and db to attempt to deliver a visual

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+### Schema
+
+CREATE DATABASE Restaurant_list;
+USE Restaurant_list;
+
+CREATE TABLE Burger_Joints
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,	
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO Burger_Joints (name) VALUES ('Matt''s Bar');
+INSERT INTO Burger_Joints (name) VALUES ('The Nook');
+INSERT INTO Burger_Joints (name) VALUES ('Groveland Tap');
