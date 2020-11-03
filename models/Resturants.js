56e08f9ba962d7f978f13eec6a41ae9dfdde6328
Tim Bluhm <tjbluhm00@gmail.com>
2020-11-03 11:47:42 -0600

    server models nad config.json

---
+++ models/Resturants.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    const Restaurant = sequelize.define("Restaurant", {
+      // Giving the Author model a name of type STRING
+      name: DataTypes.STRING
+    });
+  
+    Restaurant.associate = function(models) {
+      // Associating Restaurants with Posts
+      // When an Author is deleted, also delete any associated Posts
+      Author.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return Restaurant;
+  };
+  
