2df40c3f8eb38e631dff8309ed62e60bdc3ebff0
Tim Bluhm <tjbluhm00@gmail.com>
2020-11-05 10:18:06 -0600

    reworking get and post api routes and adding working get and put methods

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.json());
 
 // Routes
 // =============================================================
+// What does this do?
 let routes = require("./controllers/burgerController.js");
 
 app.use(routes);
