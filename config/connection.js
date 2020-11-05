09c27543c242927b68eccdad4d12d08af11d0edb
carl-33 <koepkecj@gmail.com>
2020-11-05 17:41:47 -0600

    heroku works????

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,24 @@
 // Dependencies
 var Sequelize = require("sequelize");
+// var mysql = require("mysql");
+var connection;
+if (process.env.CLEARDB_DATABASE_URL){
+  connection = new Sequelize.createConnection(process.env.CLEARDB_DATABASE_URL)
+} else {
+  var sequelize = new Sequelize("Restaurant_list", "root", "rootroot", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  })
+}
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("Restaurant_list", "root", "nelja4MS!", {
+var sequelize = new Sequelize("Restaurant_list", "root", "rootroot", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
