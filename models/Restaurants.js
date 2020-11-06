dd6c66278ca6c57cd222a329d825f25dddabbdae
Tim Bluhm <tjbluhm00@gmail.com>
2020-11-06 08:37:33 -0600

    sequelize error

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -2,7 +2,7 @@
 // =============================================================
 
 // Sequelize (capital) references the standard library
-var Sequelize = require("Sequelize");
+var Sequelize = require("sequelize");
 // sequelize (lowercase) references my connection to the DB.
 var sequelize = require("../config/connection.js");
 
